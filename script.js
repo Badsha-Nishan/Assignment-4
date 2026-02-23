501d899dcf244d76c76f0a15107030960042e8af
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-23 11:51:32 +0600

    starting logical work

---
+++ script.js
@@ -0,0 +1,10 @@
+const total = document.getElementById("total").innerText;
+const interview = document.getElementById("interview").innerText;
+const rejected = document.getElementById("rejected").innerText;
+
+const cardContainer = document.getElementById("card-container");
+console.log(cardContainer.childElementCount);
+
+function count() {
+  const card = cardContainer;
+}

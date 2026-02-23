a9201176b22fb25fa43c54dedd2f31d202bd54e9
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-23 17:07:45 +0600

    implement tab button section and first card section

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+const mainContainer = document.querySelector("main");
+
 let total = document.getElementById("total");
-let interview = document.getElementById("interview");
-let rejected = document.getElementById("rejected");
+let totalInterview = document.getElementById("interview");
+let totalRejected = document.getElementById("rejected");
+
+let interview = [];
+let rejected = [];
 
 const cardContainer = document.getElementById("card-container");
 
-function count(total) {
-  const card = cardContainer.childElementCount;
-  total.innerText = card;
-  return total;
+function count() {
+  total.innerText = cardContainer.childElementCount;
+  totalInterview.innerText = interview.length;
+  totalRejected.innerText = rejected.length;
 }
 
-count(total);
-count(interview);
+count();
+
+const allBtn = document.getElementById("allBtn");
+const interviewBtn = document.getElementById("interviewBtn");
+const rejectedBtn = document.getElementById("rejectedBtn");
+
+function tabStyle(id) {
+  allBtn.classList.remove("bg-primary", "text-white");
+  interviewBtn.classList.remove("bg-primary", "text-white");
+  rejectedBtn.classList.remove("bg-primary", "text-white");
+
+  const selected = document.getElementById(id);
+  selected.classList.add("bg-primary", "text-white");
+}
+
+mainContainer.addEventListener("click", function (event) {
+  const parent = event.target.parentNode;
+  const cardTitle = parent.querySelector(".card-title").innerText;
+});
